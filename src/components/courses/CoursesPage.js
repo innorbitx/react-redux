1e068206dc5f29d5a0b034f237890e1ea426b27e
Jose Luis Garza <josegarzaramos@gmail.com>
2021-09-14 19:04:06 -0500

    Added submit handler

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
 class CoursesPage extends React.Component {
+  state = {
+    course: {
+      title: '',
+    },
+  };
+
+  handleChange = (event) => {
+    const course = { ...this.state, title: event.target.value };
+    this.setState({ course });
+  };
+
+  handlerSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.course.title);
+  };
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form onSubmit={this.handlerSubmit}>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input type="text" onChange={this.handleChange} value={this.state.course.title} />
+
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
 
