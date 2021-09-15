cd96f216be8d87b42e04e74735fa3cc915dcc684
Jose Luis Garza <josegarzaramos@gmail.com>
2021-09-15 00:32:39 -0500

    Added redux components

---
+++ src/redux/configureStore.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './reducers';
+import reduxInmutableStateInvariant from 'redux-immutable-state-invariant';
+
+export default function configureStore(initialState) {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose; // Add support for Redux dev tools
+
+  return createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(reduxInmutableStateInvariant())));
+}
