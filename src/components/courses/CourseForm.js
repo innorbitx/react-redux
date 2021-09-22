f69c2c42b2976afea00ad24739d74b10e78aa4d0
Jose Luis Garza <josegarzaramos@gmail.com>
2021-09-22 00:58:15 -0500

    Added Jest and Enzyme tests

--- src/components/courses/CourseForm.js
+++ src/components/courses/CourseForm.js
@@ -3,7 +3,14 @@ import PropTypes from 'prop-types';
 import TextInput from '../common/TextInput';
 import SelectInput from '../common/SelectInput';
 
-const CourseForm = ({ course, authors, onSave, onChange, saving = false, errors = {} }) => {
+const CourseForm = ({
+  course,
+  authors,
+  onSave,
+  onChange,
+  saving = false,
+  errors = {},
+}) => {
   return (
     <form onSubmit={onSave}>
       <h2>{course.id ? 'Edit' : 'Add'} Course</h2>
@@ -12,7 +19,13 @@ const CourseForm = ({ course, authors, onSave, onChange, saving = false, errors 
           {errors.onSave}
         </div>
       )}
-      <TextInput name="title" label="Title" value={course.title} onChange={onChange} error={errors.title} />
+      <TextInput
+        name="title"
+        label="Title"
+        value={course.title}
+        onChange={onChange}
+        error={errors.title}
+      />
 
       <SelectInput
         name="authorId"
@@ -27,7 +40,13 @@ const CourseForm = ({ course, authors, onSave, onChange, saving = false, errors 
         error={errors.author}
       />
 
-      <TextInput name="category" label="Category" value={course.category} onChange={onChange} error={errors.category} />
+      <TextInput
+        name="category"
+        label="Category"
+        value={course.category}
+        onChange={onChange}
+        error={errors.category}
+      />
 
       <button type="submit" disabled={saving} className="btn btn-primary">
         {saving ? 'Saving...' : 'Save'}
